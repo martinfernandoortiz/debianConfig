7edcc404be960df2a6617a8739e13b90f979791c
Martín Fernando Ortiz <38224115+martinfernandoortiz@users.noreply.github.com>
2023-02-11 22:18:29 -0300

    Update mint_install.sh

--- mint_install.sh
+++ mint_install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+
 Sudo apt install zenity -y
 
 
@@ -41,3 +44,50 @@ deb-src http://qgis.org/ubuntu-ltr $(lsb_release -sc) main" > /etc/apt/sources.l
 sudo apt update
 sudo apt install qgis qgis-plugin-grass
 
+
+
+zenity ask=$(zenity --list --checklist --title='Options' --width=580 --height=600 
+       --text='Selecciona el sofware a instalar' 
+       --column='Install' 
+       --column='Software' 
+       --column='Descripción' 
+TRUE chrome-gnome-shell 'Complemento que permite la instalacion de extensiones' 
+TRUE nautilus-share 'Habilitar compartir con Windows' 
+TRUE file-roller 'Gestor de archivos comprimidos para Gnome' 
+TRUE rar 'Utilidad de compresión' 
+TRUE unrar 'Utilidad de compresión' 
+TRUE unrar-free 'Utilidad de compresión' 
+TRUE zip 'Utilidad de compresión' 
+TRUE unzip 'Utilidad de compresión'
+TRUE unace 'Utilidad de compresión' 
+TRUE unace-nonfree 'Utilidad de compresión' 
+TRUE bzip2 'Utilidad de compresión' 
+TRUE lzop 'Utilidad de compresión' 
+TRUE p7zip-full 'Utilidad de compresión' 
+TRUE p7zip-rar 'Utilidad de compresión' 
+TRUE gzip 'Utilidad de compresión' 
+TRUE lzip 'Utilidad de compresión' 
+TRUE gnome-calendar 'Calendario de Gnome' 
+TRUE vlc 'Visor Multimedia' 
+TRUE font-manager 'Gestor de tipografías' 
+TRUE eog 'Visor de archivos PDF' 
+TRUE kaffeine 'Visor multimedia' 
+TRUE gparted  'Utilidad para particionar discos' 
+TRUE menulibre 'Permite editar el menu de Gnome' 
+TRUE gnome-calculator 'Calculadora de Gnome' 
+TRUE gnome-contacts 'Gestor de contactos de Gnome' 
+TRUE gnome-weather 'El clima' TRUE ubuntu-restricted-extras 'Extras de Ubuntu' TRUE inkscape 'Editor de vectoriales' 
+TRUE gimp 'Editor de imágenes' TRUE simplescreenrecorder 'Grabador de pantalla' TRUE synaptic 'Gestor de paquetes' 
+TRUE flatpak 'Gestión de paquetes' TRUE snapd 'Gestión de paquetes' TRUE gdebi 'Gestión de paquetes' 
+TRUE mixxx 'Tornamesa de DJ' 
+TRUE breeze-cursor-theme 'Tema de cursores'
+TRUE breeze-gtk-theme 'Tema de Gnome'
+TRUE breeze-icon-theme 'Tema de iconos' 
+TRUE arc-theme 'Tema de gnome')
+instalar=""
+  if [ "$ask" != "" ] ; then
+newask=${ask//|/" "}
+     instalar="sudo apt install $newask"
+  fi
+  
+$instalar
