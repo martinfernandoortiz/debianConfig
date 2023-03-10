be39204cfcfeeaf723c2c35f23ccafafd10eaf9a
Martín Fernando Ortiz <martinfernandoortiz@gmail.com>
2023-03-09 22:58:41 -0300

    actualizacion de r

---
+++ install_Programs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+sudo apt install -y docker.io / docker-compose podman 
+
+sudo apt install -y software-properties-common wget curl
+sudo apt-get install wget
+sudo apt-get install gdebi
+
+
+sudo apt install -y pandoc				# transformar archivos
+sudo apt install -y texlive-latex-recommended   # LaTeX
+sudo apt install -y texlive-lang-spanish # paquete español 
+sudo apt install -y evince	
+sudo apt install -y calibre calibre-bin 	# ebooks
+sudo apt install -y meld              	# comparar archivos
+
+sudo apt-get install snapd
+sudo snap install blender --classic
+sudo snap remove blender
+sudo apt install -y chromium
+
+sudo apt install git-all
+wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+sudo apt update && sudo apt install github-desktop
--- install_R_debian.sh
+++ install_R_debian.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#falta agregar el repositorio para tener version mas nueva
+sudo nano /etc/apt/sources.list
+
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
+
 sudo apt install -y r-base-html build-essential #r base
 sudo apt install -y r-recommended #
+sudo apt install -t  bullseye-cran40 r-base
+
 wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2021.09.0-351-amd64.deb #bajar rstudio
 sudo apt install -f ./rstudio-2021.09.0-351-amd64.deb #instalar rStudio
 
@@ -12,3 +20,12 @@ sudo apt install -y libudunits2-dev libgdal-dev libgeos-dev libproj-dev	# sf
 sudo apt install -y protobuf-compiler libprotobuf-dev 			# tmap
 sudo apt install -y default-jdk 					# rJava (it's needed for some packages)
 sudo apt install -y libgmp-dev libmpfr-dev 				# HH
+sudo apt install libfontconfig1-dev
+sudo apt install libfribidi-dev 
+
+sudo apt install libharfbuzz-dev 
+sudo apt install libfontconfig1-dev
+
+sudo apt-get install libbz2-dev liblzma-dev
+
+
