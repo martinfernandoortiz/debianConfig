8b2560d946d59021a5f696da7c223e5df74fc1a5
Martín Fernando Ortiz <38224115+martinfernandoortiz@users.noreply.github.com>
2023-04-01 18:43:59 -0300

    Update install_Programs.sh

--- install_Programs.sh
+++ install_Programs.sh
@@ -17,11 +17,15 @@ sudo apt install -y calibre calibre-bin 	# ebooks
 sudo apt install -y meld              	# comparar archivos
 
 sudo apt-get install snapd
-sudo snap install blender --classic
+sudo snap install -y blender --classic
 sudo snap remove blender
 sudo apt install -y chromium
 
 sudo apt install git-all
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
-sudo apt update && sudo apt install github-desktop
+sudo apt update && sudo apt install -y github-desktop
+
+#Anaconda
+wget https://repo.anaconda.com/archive/Anaconda3-2023.03-Linux-x86_64.sh
+
